f2bba63f019433e47f23749ccac028331b930d7e
subhashmandal1 <subhashjarvis@gmail.com>
2024-06-05 14:30:32 +0545

    Loaded category from db in html

--- expenseswebsite/expenses/views.py
+++ expenseswebsite/expenses/views.py
@@ -1,11 +1,27 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
+from .models import Category, Expense
+from django.contrib import messages
 
 # Create your views here.
 
 @login_required(login_url='/authentication/login')
 def index(request):
+    categories=Category.objects.all()
     return render(request, 'expenses/index.html')
 
 def add_expense(request):
-    return render(request, 'expenses/add_expense.html')
+    categories=Category.objects.all()
+    context={
+        'categories':categories,
+        'values': request.POST
+    }
+    if request.method=='GET':
+        return render(request, 'expenses/add_expense.html',context)
+
+    if request.method=='POST':
+        amount= request.POST['amount']
+    
+        if not amount:
+            messages.error(request, 'Amount is requeired' )
+            return render(request, 'expenses/add_expense.html',context)
