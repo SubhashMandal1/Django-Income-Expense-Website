7ec254268df4c2b68328d4d61a759a60b7bb515c
subhashmandal1 <subhashjarvis@gmail.com>
2024-06-10 00:49:08 +0545

    Added ajax search for expenses

--- expenseswebsite/expenses/views.py
+++ expenseswebsite/expenses/views.py
@@ -5,8 +5,24 @@ from django.contrib import messages
 from django.contrib.auth.models import User
 from django.core.paginator import Paginator
 from userpreferences.models import UserPreference
+import json
+from django.http import JsonResponse
 
 # Create your views here.
+
+def search_expenses(request):
+    if request.method=="POST":
+        search_str=json.loads(request.body).get('searchText')
+        
+        expenses = Expense.objects.filter(
+            amount__istartswith=search_str, owner=request.user) | Expense.objects.filter(
+            date__istartswith=search_str, owner=request.user) | Expense.objects.filter(
+            description__icontains=search_str, owner=request.user) | Expense.objects.filter(
+            category__icontains=search_str, owner=request.user)
+        
+        data = expenses.values()
+        return JsonResponse(list(data), safe=False)
+
 
 @login_required(login_url='/authentication/login')
 def index(request):
